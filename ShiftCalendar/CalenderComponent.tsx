ec1f40cb7e9f040d0c880ca4bf2afc4637f6439a
Brenden Muldowney <brenden.muldowney@npxinnovation.ca>
2023-09-06 15:49:07 -0400

    changed react library to fullCalendar, and began implementing test events and basic CSS

--- ShiftCalendar/CalenderComponent.tsx
+++ ShiftCalendar/CalenderComponent.tsx
@@ -1,10 +1,48 @@
 import * as React from 'react';
-import Calendar from 'react-calendar';
+import { duration } from 'moment';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+
 
 
 export interface ICalendarProps {
   value: string;
 }
+
+const testjson = [
+  {
+      "title": "test shift 1",
+      "date": "2023-09-07",
+      "description": "7:00am-7:00pm",
+      "classNames": ['crew-a']
+  },
+  {
+      "title": "test shift 2",
+      "date": "2023-09-07",
+      "description": "7:00am-7:00pm",
+      "classNames": ['crew-b']
+  },
+  {
+      "title": "Crew C - Day Shift 7:00am-7:00pm",
+      "date": "2023-09-19",
+      "description": "7:00am-7:00pm",
+      "classNames": ['crew-c']
+  },
+  {
+      "title": "test shift 4",
+      "date": "2023-09-21",
+      "description": "7:00am-7:00pm",
+      "classNames": ['crew-d']
+  },
+  {
+      "title": "test shift 5",
+      "date": "2023-09-23",
+      "description": "7:00am-7:00pm",
+      "classNames": ['crew-e']
+  }
+]
+
 
 
 export class CalendarComponent extends React.Component<{}, {}>{
@@ -12,13 +50,15 @@ export class CalendarComponent extends React.Component<{}, {}>{
     return (
       <div className='component'>
         <div className='calendar-container'>
-          <Calendar minDetail="month" maxDetail="month" defaultView="month">
-          </Calendar>
+          <FullCalendar
+            plugins={[dayGridPlugin]}
+            defaultTimedEventDuration={'12:00'}
+            forceEventDuration={true}
+            initialView='dayGridMonth'
+            events={testjson}
+            />
         </div>
-
       </div>
     );
   }
 }
-
-
